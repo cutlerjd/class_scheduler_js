909dc467c4ea575bfd77eb37c26cca4670d39838
Jase Cutler <jasecutler@gmail.com>
2018-02-24 17:24:13 -0800

    Adding other.

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/other', (req,res) => res.send("You hit other!"))
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
